b1395f66f5ed931b91a87b636e66b0fd3fb46044
Tom Hicks <headhunter3@gmail.com>
2005-06-29 11:34:21 -0400

    Removed: Removed the windows specific section in preparation for a unified keyhandler. This BREAKS windows and will be fixed soon ~g2k

--- include/keys.h
+++ include/keys.h
@@ -1,77 +1,6 @@
 #ifndef __keys_h__
 #define __keys_h__
-//Everything in this file will be abstracted by oa_input
-//These definitions will be replaced at that time
-#ifdef WIN32
-#include <windows.h>
-#define KEY_LBUTTON	VK_LBUTTON
-#define KEY_RBUTTON VK_RBUTTON
-#define KEY_MBUTTON	VK_MBUTTON
-#define KEY_LEFT	VK_LEFT
-#define KEY_RIGHT	VK_RIGHT
-#define KEY_UP		VK_UP
-#define KEY_DOWN	VK_DOWN
-#define KEY_SPACE	VK_SPACE
-#define KEY_TAB		VK_TAB
-#define KEY_RETURN	VK_RETURN
-#define KEY_SHIFT	VK_SHIFT
-#define KEY_PAUSE	VK_PAUSE
-#define KEY_CONTROL	VK_CONTROL
-#define KEY_CAPITAL	VK_CAPITAL
-#define KEY_ESCAPE	VK_ESCAPE
-#define KEY_PRIOR	VK_PRIOR
-#define KEY_NEXT	VK_NEXT
-#define KEY_END		VK_END
-#define KEY_HOME	VK_HOME
-#define KEY_INSERT	VK_INSERT
-#define KEY_DELETE	VK_DELETE
-#define KEY_LWIN	VK_LWIN
-#define KEY_RWIN	VK_RWIN
-#define KEY_APPS	VK_APPS
-#define KEY_NUMPAD0	VK_NUMPAD0
-#define KEY_NUMPAD1	VK_NUMPAD1
-#define KEY_NUMPAD2	VK_NUMPAD2
-#define KEY_NUMPAD3	VK_NUMPAD3
-#define KEY_NUMPAD4	VK_NUMPAD4
-#define KEY_NUMPAD5	VK_NUMPAD5
-#define KEY_NUMPAD6	VK_NUMPAD6
-#define KEY_NUMPAD7	VK_NUMPAD7
-#define KEY_NUMPAD8	VK_NUMPAD8
-#define KEY_NUMPAD9	VK_NUMPAD9
-#define KEY_MULTIPLY	VK_MULTIPLY
-#define KEY_ADD		VK_ADD
-#define KEY_SUBTRACT	VK_SUBTRACT
-#define KEY_DIVIDE		VK_DIVIDE
-#define KEY_F1			VK_F1
-#define KEY_F2			VK_F2
-#define KEY_F3			VK_F3
-#define KEY_F4			VK_F4
-#define KEY_F5			VK_F5
-#define KEY_F6			VK_F6
-#define KEY_F7			VK_F7
-#define KEY_F8			VK_F8
-#define KEY_F9			VK_F9
-#define KEY_F10			VK_F10
-#define KEY_F11			VK_F11
-#define KEY_F12			VK_F12
-#define KEY_NUMLOCK		VK_NUMLOCK
-#define KEY_SCROLL		VK_SCROLL
-#define KEY_SEPARATOR	VK_SEPARATOR
-#define KEY_OEM_1		VK_OEM_1
-#define KEY_OEM_2		VK_OEM_2
-#define KEY_OEM_3		VK_OEM_3
-#define KEY_OEM_4		VK_OEM_4
-#define KEY_OEM_5		VK_OEM_5
-#define KEY_OEM_6		VK_OEM_6
-#define KEY_OEM_7		VK_OEM_7
-#define KEY_OEM_PERIOD	VK_OEM_PERIOD
-#define KEY_OEM_PLUS	VK_OEM_PLUS
-#define KEY_OEM_MINUS	VK_OEM_MINUS
-#define KEY_OEM_COMMA	VK_OEM_COMMA
-#define KEY_BACK		VK_BACK
-#endif
 
-#ifdef __linux
 namespace OpenArena
 {
 	enum Keys{
@@ -179,5 +108,4 @@ namespace OpenArena
 		KEY_Z
 	};
 };
-#endif
 #endif
