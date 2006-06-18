a1683beb24b158227035ffa7963330b2e5979fb3
Tom Hicks <headhunter3@gmail.com>
2006-06-18 14:34:28 -0400

    Changed Moved class ControlSchemeClass into namespace OpenArena Remived unnecessary include windows.h ~g2k

--- include/ctrls.h
+++ include/ctrls.h
@@ -2,42 +2,43 @@
 #include "physics.h"
 #include "datatypes.h"
 #include "list.h"
-#ifdef WIN32
-//#include <windows.h> //used for VK_ keys and possibly something else.  If this file wont compile try uncommenting this.
-#endif
-enum {CTRLS_FORWARD, CTRLS_BACKWARD, CTRLS_LOOKLEFT, CTRLS_LOOKRIGHT, CTRLS_LOOKUP,
-	  CTRLS_LOOKDOWN, CTRLS_MOVELEFT, CTRLS_MOVERIGHT, CTRLS_MOVEUP, CTRLS_MOVEDOWN,
-	  CTRLS_ROLLLEFT, CTRLS_ROLLRIGHT, CTRLS_FIREPRIMARY, CTRLS_FIRESECONDARY,
-	  CTRLS_WEAPONNEXT, CTRLS_WEAPONPREV, CTRLS_TOGGLE_LIGHTS, CTRLS_TOGGLE_FPS,
-	  CTRLS_TOGGLE_CONSOLE, CTRLS_TOGGLE_MOUSELOOK, CTRLS_QUICKMOUSELOOK};
 
-class ControlSchemeClass
-{
-public:
-	void LoadDefaultControlScheme();
-	void ClearControlScheme();
-	void Unbind(uint8);
-	bool Bind(uint32, uint8);
+namespace OpenArena{
 
-	list<uint8> forward;
-	list<uint8> backward;
-	list<uint8> lookLeft;
-	list<uint8> lookRight;
-	list<uint8> lookUp;
-	list<uint8> lookDown;
-	list<uint8> moveLeft;
-	list<uint8> moveRight;
-	list<uint8> moveUp;
-	list<uint8> moveDown;
-	list<uint8> firePrimary;
-	list<uint8> fireSecondary;
-	list<uint8> weaponNext;
-	list<uint8> weaponPrev;
-	list<uint8> toggleLights;
-	list<uint8> toggleFPS;
-	list<uint8> toggleConsole;
-	list<uint8> toggleMouseLook;
-	list<uint8> quickMouseLook;
-	list<uint8> rollLeft;
-	list<uint8> rollRight;	
+	enum {CTRLS_FORWARD, CTRLS_BACKWARD, CTRLS_LOOKLEFT, CTRLS_LOOKRIGHT, CTRLS_LOOKUP,
+		CTRLS_LOOKDOWN, CTRLS_MOVELEFT, CTRLS_MOVERIGHT, CTRLS_MOVEUP, CTRLS_MOVEDOWN,
+		CTRLS_ROLLLEFT, CTRLS_ROLLRIGHT, CTRLS_FIREPRIMARY, CTRLS_FIRESECONDARY,
+		CTRLS_WEAPONNEXT, CTRLS_WEAPONPREV, CTRLS_TOGGLE_LIGHTS, CTRLS_TOGGLE_FPS,
+		CTRLS_TOGGLE_CONSOLE, CTRLS_TOGGLE_MOUSELOOK, CTRLS_QUICKMOUSELOOK};
+
+	class ControlSchemeClass
+	{
+	public:
+		void LoadDefaultControlScheme();
+		void ClearControlScheme();
+		void Unbind(uint8);
+		bool Bind(uint32, uint8);
+
+		list<uint8> forward;
+		list<uint8> backward;
+		list<uint8> lookLeft;
+		list<uint8> lookRight;
+		list<uint8> lookUp;
+		list<uint8> lookDown;
+		list<uint8> moveLeft;
+		list<uint8> moveRight;
+		list<uint8> moveUp;
+		list<uint8> moveDown;
+		list<uint8> firePrimary;
+		list<uint8> fireSecondary;
+		list<uint8> weaponNext;
+		list<uint8> weaponPrev;
+		list<uint8> toggleLights;
+		list<uint8> toggleFPS;
+		list<uint8> toggleConsole;
+		list<uint8> toggleMouseLook;
+		list<uint8> quickMouseLook;
+		list<uint8> rollLeft;
+		list<uint8> rollRight;	
+	};
 };
