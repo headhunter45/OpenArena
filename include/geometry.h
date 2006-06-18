98e05cbe038481baf243aee564e8c68f5ff3fa9e
Tom Hicks <headhunter3@gmail.com>
2006-06-18 14:36:19 -0400

    Removed class Position ~g2k

--- include/geometry.h
+++ include/geometry.h
@@ -13,28 +13,3 @@ namespace OpenArena
 		Vec3f normal;
 	};
 };
-/*//This class seems to never be used and I dont' remember why I made it.
-class Position
-{
-public:
-	GLfloat xrot;			// X rotation
-	GLfloat yrot;			// Y rotation
-	GLfloat zrot;			// Z rotation
-	GLfloat xtrans;			// X translation
-	GLfloat ytrans;			// Y translation
-	GLfloat ztrans;			// Z translation
-	bool stepRight;		
-	bool stepLeft;
-	Position()
-	{
-		xtrans = 0.0f;
-		ytrans = 0.0f;
-		ztrans = 0.0f;
-		xrot = 0.0f;
-		yrot = 0.0f;
-		zrot = 0.0f;
-	}
-private:
-
-};
-*/
