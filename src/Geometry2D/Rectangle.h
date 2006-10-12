e9b37ae371e098c36ad93f1eeab72dca534ed68e
Tom Hicks <headhunter3@gmail.com>
2006-10-12 13:21:20 -0400

    Added some basic 2 dimensional geometric classes~Tom

---
+++ src/Geometry2D/Rectangle.h
@@ -0,0 +1,15 @@
+#ifndef __OpenArena_Geometry2D_Rectangle_h__
+#define __OpenArena_Geometry2D_Rectangle_h__
+
+namespace OpenArena
+{
+	namespace Geometry2D
+	{
+		class Rectangle
+		{
+				
+		};
+	};
+};
+
+#endif // __OpenArena_Geometry2D_Rectangle_h__
